0a154bd8c257f73f7303dff81f673da011f507b9
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-06 15:04:50 +0200

    added the responsive tag to the table for breakpoints and also added Icons for stars

--- src/components/MovieTable/MovieTable.jsx
+++ src/components/MovieTable/MovieTable.jsx
@@ -1,26 +1,50 @@
-import { Button, Table } from 'react-bootstrap'
+import { Button, Table } from 'react-bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function MovieTable({ movies, deleteMovie }) {
+
+  const renderRating = (rating) => {
+    return (
+      <>
+        {/* Show multiple stars on medium screens and larger */}
+        <span className="d-none d-md-inline">
+          {[...Array(Number(rating))].map((_, i) => (
+            <i key={i} className="bi bi-star-fill text-warning"></i>
+          ))}
+        </span>
+        
+        {/* Show single star with number on small screens */}
+        <span className="d-md-none">
+          <i className="bi bi-star-fill text-warning"></i> {rating}
+        </span>
+      </>
+    );
+  };
+
   return (
     <>
-      <Table hover>
+      <Table hover responsive>
         <thead>
           <tr>
             <th>Title</th>
-            <th>Rating</th>
-            <th>Action</th>
+            <th className="text-end">Rating</th>
+            <th className="text-end"></th>
           </tr>
         </thead>
         <tbody>
           {movies.map((movie) =>(
             <tr key={movie.id}>
               <td>{movie.title}</td>
-              <td>{movie.rating}</td>
-              <td>
+              <td className="text-end">{renderRating(movie.rating)}</td>
+              <td className="text-end">
                 <Button
-                variant='danger'
-                onClick={() => deleteMovie(movie.id)}
-                >Delete</Button>
+                  variant='danger'
+                  size="sm"
+                  onClick={() => deleteMovie(movie.id)}
+                >
+                  <i className="bi bi-trash"></i>
+                  <span className="d-none d-md-inline ms-1">Delete</span>
+                </Button>
               </td>
             </tr>
           ))}
