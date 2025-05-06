c340f13ac3e52faed739d3755c1e8653c59e61cc
edvintopa <55089202+edvintopa@users.noreply.github.com>
2025-05-06 11:27:13 +0200

    cleaned up css and rest of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Container, Row, Col, Button, Card } from 'react-bootstrap'
 
 function App() {
@@ -11,14 +8,7 @@ function App() {
     <Container>
       <Row className="justify-content-center mb-4">
         <Col xs={12} md={6} className="text-center">
-          <div>
-            <a href="https://vite.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
+          
           <h1 className="mt-3">Vite + React</h1>
         </Col>
       </Row>
